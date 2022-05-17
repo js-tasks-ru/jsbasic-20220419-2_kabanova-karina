b42d0a165605c7d733ed316e6501a5316234ae9f
Karina <kabanova.karina@mail.ru>
2022-05-17 09:28:10 +0300

    3/3

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,25 @@
 function initCarousel() {
-  // ваш код...
-}
+  let forward = document.querySelector('.carousel__arrow_right')
+  let back = document.querySelector('.carousel__arrow_left')
+  let carousel = document.querySelector('.carousel__inner')
+  a = 0
+  back.style.display = "none"
+
+  forward.addEventListener('click', () => {
+    back.style.display = ''
+    if (a < (carousel.offsetWidth * 3)){
+      a += carousel.offsetWidth
+    carousel.style.transform = `translateX(-${a}px)`
+  } 
+   if (a === carousel.offsetWidth * 3){ forward.style.display = 'none'
+  }})
+   
+
+  back.addEventListener('click', () => {
+    if (a > 0){
+    a -= carousel.offsetWidth
+    carousel.style.transform = `translateX(-${a}px)`}
+     
+  if (a == 0){
+    back.style.display = "none"}}
+  )}
