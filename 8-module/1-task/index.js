94235322226704bd7053dc838e038fdd2f537242
Karina <kabanova.karina@mail.ru>
2022-06-03 13:59:42 +0300

    completed

--- 8-module/1-task/index.js
+++ 8-module/1-task/index.js
@@ -26,7 +26,7 @@ export default class CartIcon {
       this.elem.classList.add('shake');
       this.elem.addEventListener('transitionend', () => {
         this.elem.classList.remove('shake');
-      }, {once: true});
+      }, { once: true });
 
     } else {
       this.elem.classList.remove('cart-icon_visible');
@@ -39,6 +39,47 @@ export default class CartIcon {
   }
 
   updatePosition() {
-    // ваш код ...
+
+    if (this.elem.offsetWidth != 57) return
+    let getInitialTopCoord = this.elem.getBoundingClientRect().top + window.pageYOffset
+    let windowpageYOffset = window.pageYOffset
+  
+
+    let leftIndent = Math.min(
+      document.querySelector('.container').getBoundingClientRect().right + 20,
+      document.documentElement.clientWidth - this.elem.offsetWidth - 10
+    ) + 'px'
+    console.log(leftIndent  +" leftIndent ")
+
+
+
+    if (windowpageYOffset > getInitialTopCoord) {
+      Object.assign(this.elem.style, {
+        position: 'fixed',
+        top: '50px',
+        zIndex: 1e3,
+        right: '10px',
+        left: leftIndent
+      });
+
+    } else {
+      Object.assign(this.elem.style, {
+        position: '',
+        top: '',
+        left: '',
+        zIndex: ''
+      });
+
+    }
+    
+    if (document.documentElement.clientWidth <= 767) {
+      Object.assign(this.elem.style, {
+        position: '',
+        top: '',
+        left: '',
+        zIndex: ''
+      });
+    }
+
   }
 }
